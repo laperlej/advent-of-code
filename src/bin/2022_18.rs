37633fa4f13d2d3762ea4af9f1af4963ba74ddfc
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2022-12-18 18:15:12 -0500

    slightly faster

--- src/bin/2022_18.rs
+++ src/bin/2022_18.rs
@@ -63,18 +63,14 @@ impl FromStr for Grid {
 impl Grid {
     fn sides(&self) -> i64 {
         let mut sides = 0;
-        for x in 1..self.grid.shape()[0]-1 {
-            for y in 1..self.grid.shape()[1]-1 {
-                for z in 1..self.grid.shape()[2]-1 {
-                    if self.grid[[x,y,z]] == 1 {
-                        if self.grid[[x+1,y,z]] == 0 { sides += 1; }
-                        if self.grid[[x-1,y,z]] == 0 { sides += 1; }
-                        if self.grid[[x,y+1,z]] == 0 { sides += 1; }
-                        if self.grid[[x,y-1,z]] == 0 { sides += 1; }
-                        if self.grid[[x,y,z+1]] == 0 { sides += 1; }
-                        if self.grid[[x,y,z-1]] == 0 { sides += 1; }
-                    }
-                }
+        for ((x,y,z), e) in self.grid.indexed_iter() {
+            if *e == 1 {
+                if self.grid[[x+1,y,z]] == 0 { sides += 1; }
+                if self.grid[[x-1,y,z]] == 0 { sides += 1; }
+                if self.grid[[x,y+1,z]] == 0 { sides += 1; }
+                if self.grid[[x,y-1,z]] == 0 { sides += 1; }
+                if self.grid[[x,y,z+1]] == 0 { sides += 1; }
+                if self.grid[[x,y,z-1]] == 0 { sides += 1; }
             }
         }
         sides
